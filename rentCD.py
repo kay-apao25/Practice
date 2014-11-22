f35ff70a6c525ed54616ad4dd346b0cccc884063
kay-apao25 <kayapao@yahoo.com>
2014-11-22 20:22:44 +0800

    added add_customer method in rentCD.py and new tests in rentCD_test.py

--- rentCD.py
+++ rentCD.py
@@ -1,5 +1,10 @@
+from customer import Customer
+
 class RentCD(object):
 
     def __init__ (self):
         self.customer_list = {}
         self.cd_list = {"1", "2", "3", "4", "5"}
+
+    def add_customer(self, customer):
+        self.customer_list[customer.id] = customer.id 
--- rentCD_test.py
+++ rentCD_test.py
@@ -15,3 +15,26 @@ class rentCDTest(unittest.TestCase):
     def test_cd_list_is_not_initially_empty(self):
         self.assertEqual({"1", "2", "3", "4", "5"}, self.rent_cd.cd_list)
         self.assertGreater(len(self.rent_cd.cd_list), 0)
+
+    def test_add_customer(self):
+
+        customer_1 = Customer("1")
+        customer_2 = Customer("2")
+        
+        self.rent_cd.add_customer(customer_1)
+        self.rent_cd.add_customer(customer_2)
+   
+        self.assertEqual(len(self.rent_cd.customer_list),2)
+
+    def test_customer_does_exist(self):
+        
+        customer_1 = Customer("1")
+        customer_2 = Customer("2")
+ 
+        self.rent_cd.add_customer(customer_1)
+        self.rent_cd.add_customer(customer_2)
+
+        self.assertIn("2",self.rent_cd.customer_list)
+
+    def test_cd_does_exity(self):
+        self.assertIn("5",self.rent_cd.cd_list)
